f2f9ceaf63b5a075967408014ea66a9cb1da26a4
Jonathan Windridge <jonathan.windridge@gmail.com>
2014-10-04 12:00:38 +0100

    Added models for Event and Person

---
+++ EuromoneyHackathon/Models/Event.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EuromoneyHackathon.Models
+{
+    public class Event
+    {
+        public string eventId { get; set; }
+        public string eventName { get; set; }
+        /// <summary>
+        /// Create event, generating new random eventId
+        /// </summary>
+        public Event()
+        {
+            this.eventId = Guid.NewGuid().ToString().Replace("{", "").Replace("}", "").Replace("-", "");
+        }
+
+        /// <summary>
+        /// Create event, specifing eventId (used when initialising event class from data)
+        /// </summary>
+        /// <param name="eventId"></param>
+        public Event(string eventId)
+        {
+            this.eventId = eventId;
+        }
+    }
+}
---
+++ EuromoneyHackathon/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EuromoneyHackathon.Models
+{
+    public class Person
+    {
+        public string personId { get; set; }
+        public string personFirstName { get; set; }
+        public string personLastName { get; set; }
+
+        public Person()
+        {
+            this.personId = Guid.NewGuid().ToString().Replace("{", "").Replace("}", "").Replace("-", "");
+        }
+
+        public Person(String personId)
+        {
+            this.personId = personId;
+        }
+    }
+}
