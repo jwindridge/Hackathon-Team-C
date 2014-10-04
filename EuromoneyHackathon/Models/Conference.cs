d4e97ed7cd0603ba4ab91fe5d17022e010265bd5
GuoweiMO <moguowei0730@gmail.com>
2014-10-04 12:25:28 +0100

    add the Conference model

---
+++ EuromoneyHackathon/Models/Conference.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EuromoneyHackathon.Models
+{
+    public class Conference
+    {
+        public string conferenceId { get; set; }
+        public string conferenceName { get; set; }
+        
+        /// <summary>
+        /// create new conference ID
+        /// </summary>
+        public Conference() 
+        {
+            String rawId = Guid.NewGuid().ToString();
+            rawId = rawId.Replace("{", "").Replace("}", "").Replace("-", "");
+            conferenceId = rawId;
+        }
+        /// <summary>
+        /// create Conference form id
+        /// </summary>
+        /// <param name="confId"></param>
+        public Conference(string conferenceId)
+        {
+            this.conferenceId = conferenceId;
+        }
+
+    }
+}
