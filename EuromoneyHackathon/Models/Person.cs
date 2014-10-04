b5e3a62aac05baa17573327a0bce41ce42fa1b93
Jonathan Windridge <jonathan.windridge@gmail.com>
2014-10-04 14:03:48 +0100

    Added PersonController and basic methods
    -  Added methods to PersonController to return static list of people, as
       well as find specific people by id

--- EuromoneyHackathon/Models/Person.cs
+++ EuromoneyHackathon/Models/Person.cs
@@ -11,14 +11,28 @@ namespace EuromoneyHackathon.Models
         public string personFirstName { get; set; }
         public string personLastName { get; set; }
 
-        public Person()
-        {
-            this.personId = Guid.NewGuid().ToString().Replace("{", "").Replace("}", "").Replace("-", "");
-        }
-
-        public Person(String personId)
+        /// <summary>
+        /// Create person, specifying id, first name & last name
+        /// </summary>
+        /// <param name="personId">id of person</param>
+        /// <param name="firstName">Person's first name</param>
+        /// <param name="lastName">Person's last name</param>
+        public Person(string personId, string firstName, string lastName)
         {
             this.personId = personId;
+            this.personFirstName = firstName;
+            this.personLastName = lastName;
+        }
+        /// <summary>
+        /// Create person, generating id at random, but specifying name attributes
+        /// </summary>
+        /// <param name="firstName">Person's first name</param>
+        /// <param name="lastName">Person's last name</param>
+        public Person(string firstName, string lastName)
+        {
+            this.personId = Guid.NewGuid().ToString().Replace("{", "").Replace("}", "").Replace("-", "");
+            this.personFirstName = firstName;
+            this.personLastName = lastName;
         }
     }
 }
